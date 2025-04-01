7cf59aa33017e561d5441e13806e3f35d10e90fd
aman-selldo <aman.singh@sell.do>
2025-04-01 11:30:04 +0530

    latest commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+
+  root to: "books#index"  
   resources :libraries do
     resources :library_books, only: [:create, :destroy, :index, :new]
   end
 
   resources :books
- 
 end
